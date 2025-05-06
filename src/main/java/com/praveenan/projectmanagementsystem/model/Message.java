5c3fef34cfb61eabfabb0d171835d98e654c0bc8
Praveenan Jeevarethinam <praveenanjvp@gmail.com>
2025-05-06 12:49:54 +0530

    SignIn and SignUp functionalities has Implemented

---
+++ src/main/java/com/praveenan/projectmanagementsystem/model/Message.java
@@ -0,0 +1,32 @@
+package com.praveenan.projectmanagementsystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String content;
+  private LocalDateTime createdAt;
+
+  @ManyToOne
+  private Chat chat;
+
+  @ManyToOne
+  private User sender;
+
+}
